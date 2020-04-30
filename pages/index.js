ec29dce56dc642929270651c9945cc53b7adab01
Fernando Poncio <fecrackle@gmail.com>
2020-04-30 00:49:01 -0600

    Agregando primera pagina

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default class extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hola mundo</h1>
+        <p>Aprendiendo a programar en Next.js con React.js</p>
+      </React.Fragment>
+    );
+  }
+}
